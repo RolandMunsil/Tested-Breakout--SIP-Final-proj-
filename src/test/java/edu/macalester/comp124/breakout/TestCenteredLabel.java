351658b3ae7ca4d9031611a1b5f20b15d61df2e8
clgzb9610 <wkim@macalester.edu>
2018-12-15 16:05:41 -0600

    created test for CenteredLabel

--- src/test/java/edu/macalester/comp124/breakout/TestCenteredLabel.java
+++ src/test/java/edu/macalester/comp124/breakout/TestCenteredLabel.java
@@ -1,22 +1,33 @@
 package edu.macalester.comp124.breakout;
 
 import acm.graphics.GLabel;
+import acm.graphics.GPoint;
 import acm.program.GraphicsProgram;
+import java.awt.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.assertEquals;
 
-import static org.mockito.Mockito.*;
 
-import static org.mockito.Mockito.times;
+public class TestCenteredLabel extends GraphicsProgram{
+    GLabel testLabel = new GLabel("");
+    GraphicsProgram graphicsProgram = new GraphicsProgram() {};
+    CenteredLabel centeredLabel;
+    int EXPECTED_FONT_SIZE = 10;
 
-public class TestCenteredLabel {
+    private void constructCenteredLabel() {
+        centeredLabel = new CenteredLabel(graphicsProgram, 100, 100, EXPECTED_FONT_SIZE);
+    }
 
     @Test
-    public void constructorAddsLabelToGraphicsProgram() {
-        GraphicsProgram mockGraphicsProgram = mock(GraphicsProgram.class);
-        CenteredLabel centeredLabel = new CenteredLabel(mockGraphicsProgram, 100, 100, 10);
-        verify(mockGraphicsProgram, times(1)).add(isA(GLabel.class));
+    public void constructorAddsLabelToGraphicsProgram(){
+        constructCenteredLabel();
+        assertEquals(testLabel.getClass(), graphicsProgram.getElement(0).getClass());
     }
+
+    @Test
+    public void constructorSetsFontSize(){
+        constructCenteredLabel();
+    }
+
 }
